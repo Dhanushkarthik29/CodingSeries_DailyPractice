3a7f7f9019cb8fb961655c0f3a32b3cc7dc98f0b
Dhanush Krishna K <85447114+Dhanushkarthik29@users.noreply.github.com>
2026-01-08 15:17:47 +0530

    Create InOrder.java

---
+++ Concepts/Tree/BinaryTree/InOrder.java
@@ -0,0 +1,34 @@
+//Definition for a binary tree node.
+// LEFT - ROOT - RIGHT
+//--------------------------------------
+ public class TreeNode {
+      int data;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int val) { data = val; left = null, right = null }
+ }
+ class Solution {
+    public List<Integer> inorder(TreeNode root) {
+        TreeNode node=root;
+        Stack<TreeNode> st=new Stack<TreeNode>();
+        List<Integer> l=new ArrayList<Integer>();
+        while(true){
+            if(node!=null){
+                st.push(node);
+                //System.out.println(node.data);
+                node=node.left;
+            }
+            else{
+                if(st.empty()){
+                    break;
+                }
+                node=st.pop();
+                //System.out.println(node.data+"*");
+                l.add(node.data);
+                node=node.right;
+                //System.out.println(node.right+"**");
+            }
+        }
+        return l;
+    }
+}
